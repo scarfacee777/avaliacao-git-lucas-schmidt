27101577a8a6b38a63761af9c6b900b325b03166
LUCAS GUILHERME SCHMIDT <lucas.schmidt@alunos.sc.senac.br>
2025-07-07 20:11:28 -0300

    Atualizado

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Avaliação Git com Java");
+
+        Calculadora calc = new Calculadora();
+
+        System.out.println("Soma: " + calc.soma(10, 5));
+        System.out.println("Multiplicação: " + calc.multiplicacao(10, 5));
+        System.out.println("Divisão: " + calc.divisao(10, 5));
     }
 }
-
